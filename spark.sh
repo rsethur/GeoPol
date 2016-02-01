6658aea6c0528293c360277a3d3c338899019c2f
rsethur <rsethur@gmail.com>
2016-02-01 22:27:26 +0400

    initial

---
+++ spark.sh
@@ -0,0 +1,8 @@
+#export PYSPARK_PYTHON=/c/DevLibs/spark-1.5.2-bin-hadoop2.6
+export SPARK_HOME=/c/DevLibs/spark-1.6.0-bin-hadoop2.6
+export PYSPARK_DRIVER_PYTHON="ipython"
+export PYSPARK_PYTHON=/c/DevLibs/Anaconda/python
+export PYSPARK_DRIVER_PYTHON_OPTS="notebook"
+export HADOOP_HOME=/c/DevLibs/winutils/
+#pyspark.cmd --packages com.databricks:spark-csv_2.10:1.3.0
+pyspark.cmd
